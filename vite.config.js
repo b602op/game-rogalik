1ed9ed1ef7c8b43fafea097f584ee1ea649c9aa1
Eduard Ispolatov <eispolatov@MacBook-Air.local>
2025-08-11 11:33:17 +0300

    add single html

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
-export default defineConfig({
-  base: './',
+export default defineConfig(({ command, mode }) => {
+  const isSingleFileBuild = mode === 'singlefile';
+
+  return {
+    base: './',
+    plugins: isSingleFileBuild ? [viteSingleFile()] : [],
+    build: {
+      outDir: isSingleFileBuild ? 'single-index' : '',
+    },
+  };
 })
